e8edf1b3723d0a76d74fae12f6de57c0b4e83f7d
blaise <bsjbrp@gmail.com>
2022-10-29 10:19:39 +1100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,20 @@
+public class Client {
+
+    public static void main(String[] args) {
+        showScreen();
+
+    }
+
+    public static void showScreen(){
+
+        System.out.println(cell());
+
+    }
+
+    public static String cell (){
+
+        String cell = "***\n*X*\n***";
+
+        return cell;
+    }
+}
