fbf268bc8066ed849632ce230bc9109ff2115b11
blaise <bsjbrp@gmail.com>
2022-10-29 11:04:21 +1100

    Message 564

--- src/Client.java
+++ src/Client.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Client {
 
     public static void main(String[] args) {
+        Scanner coolScanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
+        Scanner charlieScanner = new Scanner(System.in);
+
+        System.out.println("Hello Welcome To Naughts and Crosses");
+        System.out.println("Do you want to play against your friend or the computer? (y/n)");
+        String input = charlieScanner.nextLine();
+        boolean playingWithComputer;
+
+        if (input.equals("y")){
+            playingWithComputer = true;
+        } else {
+            playingWithComputer = false;
+        }
+
+        boolean coolBoolean = true;
+        boolean myBoolean = true;
+        System.out.println("Do you want to be naughts or crosses? (y/n)");
+        input = coolScanner.nextLine();
+
         showScreen();
+
+
 
     }
 
