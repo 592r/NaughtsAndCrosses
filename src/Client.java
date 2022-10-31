19879281be9647f21ff2091d92658fffd4ca4874
Charlie Howcroft <81503240+charlieHowcroft@users.noreply.github.com>
2022-10-31 21:35:22 +1100

    Update Client.java
    Add TODOs

--- src/Client.java
+++ src/Client.java
@@ -9,8 +9,11 @@ public class Client {
 
         System.out.println("Hello Welcome To Naughts and Crosses");
         System.out.println("Do you want to play against the computer? (y/n)");
+        
         String input = charlieScanner.nextLine();
         boolean playingWithComputer;
+        
+        int round = 0;
 
         if (input.equals("y")){
             playingWithComputer = true;
@@ -18,16 +21,39 @@ public class Client {
             playingWithComputer = false;
         }
         
-        /*
-        #Some stuff I did
-        if (playingWithComputer = true;){
+        
+        //Some stuff I did
+        // Nice one, to check if something is equal in java we have to use .equals(). I changed it below
+        if (playingWithComputer.equals(true)){
             
             
         } else {
             
+            // #TODO Ask for player one name and if they want naughts or crosses
+            
+            // #TODO Ask for player two name
+            
+            while (gameOver.equals(false)){
+                
+                if(round % 2 == 0){
+                    // Even round number, player one's turn
+                    
+                    // #TODO Ask where player two wants to put their move (row then column)
+                    
+                } else {
+                    
+                    //Odd Round Number, player two's turn
+                    
+                    // #TODO Ask where player two wants to put their move
+                }
+                   
+                
+            }
+            
             
         }
-        */
+        
+        
         boolean coolBoolean = true;
         boolean myBoolean = true;
         System.out.println("Do you want to be naughts or crosses? (y/n)");
